dd0509882244392aee1c52539c28b741f73946fd
slug <malcolmtan2095@gmail.com>
2026-03-27 23:37:08 +0000

    strogatzing it up

--- wiki/strogatz/code/c2/rk4test.py
+++ wiki/strogatz/code/c2/rk4test.py
@@ -15,7 +15,7 @@ def f(x):
 def rk4logistic(init_cons,dt):
     '''
     simulate logistic equation dxdt=x(1-x)
-    init_cons: vector of initial conditions (t_0,x_0)
+    init_cons: vector of initial conditions (t_0,x_0) (for now assume t_0=0)
     dt: timestep
     '''
     #timevec
@@ -23,15 +23,24 @@ def rk4logistic(init_cons,dt):
     #vector for values
     xvec=np.zeros((np.shape(init_cons)[0],np.shape(tvec)[0])) 
 
+
+
     
     #rk4
-    for cond in trange(len(init_cons)):
-        
-        for t in range(len(tvec)):
+    #for t in range(len(tvec)):
         
 
 
 
 
+X=np.arange(0,30,2)
+Y=np.arange(0,30,2)
 
-rk4logistic([[0,1]],0.1)
+
+fig,ax=plt.subplots()
+U,V=np.meshgrid(X,Y)
+plt.plot(U,V, marker='o',color='k',linestyle='none')
+plt.show()
+
+
+
