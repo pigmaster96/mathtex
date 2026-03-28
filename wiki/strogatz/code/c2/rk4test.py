63d30090c1343b2c1a1b4487011b84e7d082d32e
slug <malcolmtan2095@gmail.com>
2026-03-28 20:12:15 +0000

    phase portrait

--- wiki/strogatz/code/c2/rk4test.py
+++ wiki/strogatz/code/c2/rk4test.py
@@ -12,7 +12,7 @@ def f(x):
 
 
 
-def rk4logistic(init_cons,dt):
+def rk4logistic(init_x,dt):
     '''
     simulate logistic equation dxdt=x(1-x)
     init_cons: vector of initial conditions (t_0,x_0) (for now assume t_0=0)
@@ -21,25 +21,36 @@ def rk4logistic(init_cons,dt):
     #timevec
     tvec=np.arange(0,30,dt)
     #vector for values
-    xvec=np.zeros((np.shape(init_cons)[0],np.shape(tvec)[0])) 
-
-
-
-    
+    xvec=np.zeros(np.shape(tvec)[0]) 
+    #set init
+    xvec[0]=init_x
+ 
     #rk4
-    #for t in range(len(tvec)):
-        
+    for t in range(len(tvec)-1):
+        k_1=f(xvec[t])*dt
+        k_2=f(xvec[t]+0.5*k_1)*dt
+        k_3=f(xvec[t]+0.5*k_2)*dt
+        k_4=f(xvec[t]+k_3)*dt
+        xvec[t+1]=xvec[t]+(1/6)*(k_1+2*k_2+2*k_3+k_4)
+
+
+    return xvec, tvec
+
+ 
 
 
 
+x,t=rk4logistic(0.5,0.2)
 
-X=np.arange(0,30,2)
-Y=np.arange(0,30,2)
+
+X=np.arange(0,30,3)
+Y=np.arange(0,5,0.5)
 
 
 fig,ax=plt.subplots()
 U,V=np.meshgrid(X,Y)
 plt.plot(U,V, marker='o',color='k',linestyle='none')
+plt.plot(t,x)
 plt.show()
 
 
---
+++ wiki/strogatz/code/c2/rk4testv2.py
@@ -0,0 +1,72 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import trange
+
+
+def f(x):
+    '''
+    logistic model dxdt=x(1-x)
+    '''
+    return x*(1-x)
+
+
+
+def rk4logistic(init_cons,dt):
+    '''
+    simulate logistic equation dxdt=x(1-x)
+    init_cons: vector of initial conditions (t_0,x_0) (integers)
+    dt: timestep
+    '''
+    #timevec
+    tvec=np.arange(0,10,dt)
+    #vector for values
+    xvec=np.zeros((np.shape(init_cons)[0],np.shape(tvec)[0])) 
+
+    for con in range(np.shape(init_cons)[0]):
+        #set initial condition
+        t_0=int(np.round(init_cons[con][0]/dt))
+        xvec[con,0:t_0]=np.nan
+        xvec[con,t_0]=init_cons[con][1] 
+        
+
+        #rk4
+        for t in range(len(tvec)-1-t_0):
+            k_1=f(xvec[con,t_0+t])*dt
+            k_2=f(xvec[con,t_0+t]+0.5*k_1)*dt
+            k_3=f(xvec[con,t_0+t]+0.5*k_2)*dt
+            k_4=f(xvec[con,t_0+t]+k_3)*dt
+            xvec[con,t_0+t+1]=xvec[con,t_0+t]+(1/6)*(k_1+2*k_2+2*k_3+k_4)
+
+    return tvec, xvec
+
+ 
+
+
+#compute
+t,x=rk4logistic([(0,0.5),(1,1.4),(2,0.3),(0,0.003)],0.1)
+
+#phase portrait
+X=np.arange(0,1.5,0.1)
+T=np.arange(0,10,0.5)
+
+traj_x=f(X)
+traj_t=np.ones(np.shape(T))*0.1
+
+#position
+T,X=np.meshgrid(T,X)
+#gradient at each position
+traj_t,traj_x=np.meshgrid(traj_t,traj_x)
+
+
+
+fig,ax=plt.subplots()
+
+ax.quiver(T,X,traj_t,traj_x,pivot='tail',scale=5,headwidth=1)
+
+
+for traj in range(np.shape(x)[0]):
+    plt.plot(t,x[traj,:],linewidth=3)
+plt.show()
+
+
+
